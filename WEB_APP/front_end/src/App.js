389a9a88a4b8d88552f513ac98bd90ac767f9c9d
ReedKrawiec <reedkrawiec@gmail.com>
2021-11-09 02:18:45 -0500

    Basic socket.io implementation

--- WEB_APP/front_end/src/App.js
+++ WEB_APP/front_end/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import { io } from "socket.io-client";
+import React, { useState, useEffect } from 'react';
 function generateCode(){
   let characters = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   return Array.from(new Array(6)).map(() => characters[Math.floor(Math.random() * characters.length)]).join("");
 }
+
+Notification.requestPermission(function(status) {
+  console.log('Notification permission status:', status);
+});
+
+const socket = io();
+
+socket.on('message', function (message) {
+  console.log(message);
+});
 
 const App = () => {
   let code;
@@ -14,8 +25,11 @@ const App = () => {
   else{
     code = (localStorage.getItem("code"));
   }
-  console.log(code);
-  fetch(`/register/${code}`);
+  
+  useEffect(()=>{
+    socket.send(code);
+  },[code])
+
   return (
     <div>
       Hello! Code: {code}
