5c78a54426710bde48dbe6e879a02584f24f6e31
ReedKrawiec <reedkrawiec@gmail.com>
2021-12-02 02:15:10 -0500

    Branding / Hamburger Icon Resources

--- ANDROID_APP/app/src/main/java/io/github/reedkrawiec/buzz/BaseActivity.kt
+++ ANDROID_APP/app/src/main/java/io/github/reedkrawiec/buzz/BaseActivity.kt
@@ -1,10 +1,15 @@
 package io.github.reedkrawiec.buzz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 
 open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val ImageView = findViewById<ImageView>(R.id.buzz_icon)
+
     }
 }
