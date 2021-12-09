df7d79bea0c2f6e75f8dd4c8e5e2017a94c6519f
Reed Krawiec <reedkrawiec@gmail.com>
2021-12-09 02:26:11 -0500

    Fixed front end sending too many notifications

--- WEB_APP/front_end/src/App.js
+++ WEB_APP/front_end/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { io } from "socket.io-client";
 import { ReactComponent as Clock } from "./Clock.svg"
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useRef } from 'react';
 function generateCode(){
   let characters = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   return Array.from(new Array(6)).map(() => characters[Math.floor(Math.random() * characters.length)]).join("");
@@ -43,7 +43,8 @@ const App = () => {
     code = (localStorage.getItem("code"));
   }
   
-  const [useAlerts,setAlerts] = useState([]);
+  const alerts = useRef([]);
+  const [useAlerts,_setAlerts] = useState([]);
   const [hasPaired,setPaired] = useState(false)
   useEffect(()=>{
     socket.on('message', function (message) {
@@ -55,24 +56,27 @@ const App = () => {
               icon:"./logo192.png"
             });
             let now = new Date();
-            setAlerts([now.toLocaleTimeString(),...useAlerts]);
+            
+            alerts.current = [now.toLocaleTimeString(),...alerts.current];
+            _setAlerts(alerts.current);
           }
         });
       } else if(message == "paired"){
         setPaired(true);
       }
     });
-  })
+  },[]);
 
   useEffect(()=>{
     socket.send(code);
-  },[code])
+  },[code]);
 
   return (
     <div>
       <div className="contentContainer">
         <p className="contentLines">Your code is <span className="code">{code}</span></p>
-        <PairNotice paired={hasPaired} />        
+        <PairNotice paired={hasPaired} />
+        {useAlerts.current}        
         <div className="alertsContainer">
           {useAlerts.map((time)=><Alert time={time}/>)}
         </div>
